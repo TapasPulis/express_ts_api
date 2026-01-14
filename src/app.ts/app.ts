e2364f220b1072de834a927029162eee637f911e
TapasPulis <rooneysamragya10@gmail.com>
2026-01-14 14:18:00 +0100

    Created a new branch with some routing and controllers stuff

--- src/app.ts/app.ts
+++ src/app.ts/app.ts
@@ -1,9 +1,11 @@
 import express, { type Request, type Response } from "express";
+import userRoutes from "../routes/user.routes";
 
 export const createApp = () => {
   const app = express();
   app.use(express.json());
 
+  app.use("/api/users", userRoutes);
   app.get("/health", (req: Request, res: Response) => {
     res.status(200).json({ status: "ok", timestamp: new Date().toISOString() });
   });
