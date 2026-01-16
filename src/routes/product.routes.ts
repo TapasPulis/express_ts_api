1c0bb5fbbfd37d4e060b58905e1a40babc96f1f2
TapasPulis <rooneysamragya10@gmail.com>
2026-01-16 12:44:00 +0100

    Completed the services and logic exercise

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { getAllProducts } from "../controllers/product.controller";
-import { getProductById } from "../controllers/product.controller";
+import {
+  createProduct,
+  getAllProducts,
+  getProductById,
+} from "../controllers/product.controller";
 
 const router = Router();
 
 router.get("/", getAllProducts);
 router.get("/:id", getProductById);
+router.post("/", createProduct);
 
 export default router;
