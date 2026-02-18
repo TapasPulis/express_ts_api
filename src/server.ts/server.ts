47ea7ec3d08e4256c3595a46d3ad91aa9fd57747
TapasPulis <rooneysamragya10@gmail.com>
2026-02-18 14:41:24 +0100

    Connected express to SQL

--- src/server.ts/server.ts
+++ src/server.ts/server.ts
@@ -1,9 +1,16 @@
 import dotenv from "dotenv";
 import { createApp } from "../app.ts/app";
+import { pool } from "../config/db";
+
 dotenv.config();
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
 const app = createApp();
+
+pool
+  .connect()
+  .then(() => console.log("DB connected✅"))
+  .catch((e) => console.log(e));
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
