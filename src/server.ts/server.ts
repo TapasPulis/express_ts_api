480fe801090b969515825adc9c2b889d4e4620f0
TapasPulis <rooneysamragya10@gmail.com>
2026-02-20 14:15:16 +0100

    Updated main to use SQL instead of mongoose

--- src/server.ts/server.ts
+++ src/server.ts/server.ts
@@ -1,17 +1,35 @@
+// src/server.ts
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import { createApp } from "../app.ts/app";
 import { pool } from "../config/db";
 
 dotenv.config();
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
-const app = createApp();
+const NODE_ENV = process.env.NODE_ENV || "development";
+const MONGO_URI = process.env.MONGO_URI ?? "mongodb://localhost:27017/myapp";
 
-pool
-  .connect()
-  .then(() => console.log("DB connected✅"))
-  .catch((e) => console.log(e));
+const startServer = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.info("✅ Connected to MONGO DB - Congrats!");
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+    const app = createApp();
+    pool
+      .connect()
+      .then(() => console.log("DB connected"))
+      .catch((e) => console.log(e));
+
+    app.listen(PORT, () => {
+      console.log(
+        `🚀 Server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`,
+      );
+    });
+  } catch (error) {
+    console.error("❌ Failed to start the server", error);
+    process.exit(1);
+  }
+};
+
+startServer();
